5b89e706692ef8ced8a0998eaa8f014f9af0d21a
YouRekt <czarnecki.jurko@gmail.com>
2025-02-04 05:58:31 +0100

    refactor: remove UserContext and UserProvider, simplify routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
 import LoginForm from "@/components/LoginForm";
-import { UserContext } from "@/UserContext";
-import { useContext } from "react";
-import { Navigate } from "react-router";
 
 export default function App() {
-	const userContext = useContext(UserContext);
-
-	if (userContext?.user) {
-		return <Navigate to="/cars" replace />;
-	}
-
 	return (
 		<main className="w-full h-dvh flex flex-col items-center justify-start pt-8 gap-8">
 			<img src="/logo.svg" alt="logo" width={720} />
