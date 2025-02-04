3b187eeef729188ea8fa8857e83a7b6f51a10d20
YouRekt <czarnecki.jurko@gmail.com>
2025-02-04 05:58:44 +0100

    refactor: remove UserContext and UserProvider components, delete old DataTable implementation

--- src/UserContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-interface UserContextType {
-	user: string | null;
-	login: (user: string) => void;
-	logout: () => void;
-}
-
-export const UserContext = createContext<UserContextType | null>(null);
