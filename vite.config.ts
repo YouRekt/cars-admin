ff0b7b9e6bdfa2b62bd05d6dc23796c2e134ca40
YouRekt <czarnecki.jurko@gmail.com>
2025-02-06 16:04:27 +0100

    feat: add Vercel rewrites configuration and comment out proxy logging in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -20,24 +20,24 @@ export default defineConfig({
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
 
-				configure: (proxy, _options) => {
-					proxy.on("error", (err, _req, _res) => {
-						console.log("error", err);
-					});
-					proxy.on("proxyReq", (_proxyReq, req, _res) => {
-						console.log(
-							"Request sent to target:",
-							req.method,
-							req.url
-						);
-					});
-					proxy.on("proxyRes", (proxyRes, req, _res) => {
-						console.log(
-							"Response received from target:",
-							proxyRes.statusCode,
-							req.url
-						);
-					});
+				// configure: (proxy, _options) => {
+				// 	proxy.on("error", (err, _req, _res) => {
+				// 		console.log("error", err);
+				// 	});
+				// 	proxy.on("proxyReq", (_proxyReq, req, _res) => {
+				// 		console.log(
+				// 			"Request sent to target:",
+				// 			req.method,
+				// 			req.url
+				// 		);
+				// 	});
+				// 	proxy.on("proxyRes", (proxyRes, req, _res) => {
+				// 		console.log(
+				// 			"Response received from target:",
+				// 			proxyRes.statusCode,
+				// 			req.url
+				// 		);
+				// 	});
 				},
 			},
 		},
