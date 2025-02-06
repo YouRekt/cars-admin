7823440c09cf29c6acb1bbdce66fc9295c0e4f1d
Mateusz Małachowski <01180667@pw.edu.pl>
2025-02-06 01:47:46 +0100

    feat: add car edit form

---
+++ src/components/cars/EditCarForm.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { DialogClose } from "@/components/ui/dialog";
+import useAuth from "@/hooks/use-auth";
+import { useToast } from "@/hooks/use-toast";
+
+const formSchema = z.object({
+    modelId: z.string().uuid(),
+    locationId: z.string().uuid(),
+    imageId: z.string().uuid(),
+});
+
+const EditCarForm = ({
+	setCarAdded,
+    modelId,
+    locationId,
+    imageId,
+}: {
+	setCarAdded: (next: boolean) => void;
+	modelId: string;
+    locationId: string;
+    imageId: string;
+}) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            modelId: modelId,
+            locationId: locationId,
+            imageId: imageId,
+        },
+    });
+    const [id] = useAuth();
+    const { toast } = useToast();
+
+	async function onSubmit(values: z.infer<typeof formSchema>) {
+		const response = await fetch(`/api/cars/${modelId}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${id}`,
+			},
+			body: JSON.stringify(values),
+		});
+
+		if (response.ok) {
+			form.reset();
+		}
+
+		toast({
+			title: "Car information edited",
+			description: `Car's ${modelId} modelId has been changed to ${values.modelId} successfully.`,
+		});
+
+		setCarAdded(true);
+	}
+
+	return (
+		<Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="modelId"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Model ID</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="00000000-0000-0000-0000-000000000000"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                This is the car's ID.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="locationId"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Location ID</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="00000000-0000-0000-0000-000000000000"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                This is the car's Location ID.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="imageId"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Image ID</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="00000000-0000-0000-0000-000000000000"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                This is the car's image ID.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <div className="flex justify-between">
+                    {form.formState.isValid ? (
+                        <DialogClose>
+                            <Button type="submit">Submit</Button>
+                        </DialogClose>
+                    ) : (
+                        <Button disabled>Submit</Button>
+                    )}
+                    <DialogClose>
+                        <Button variant="destructive">Close</Button>
+                    </DialogClose>
+                </div>
+            </form>
+        </Form>
+	);
+};
+export default EditCarForm;
--- src/components/cars/columns.tsx
+++ src/components/cars/columns.tsx
@@ -19,6 +19,8 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 
+import EditCarForm from "./EditCarForm";
+
 export type Model = {
     id: string
     brandName: string
@@ -43,127 +45,140 @@ export type Car = {
     model: Model
     location: Location
     imageUrl: string
+    imageId: string
 }
 
 export const columns = (
     handleDelete: (id: string) => void,
+    setCarAdded: (next: boolean) => void
 ): ColumnDef<Car>[] => [
-    {
-        accessorKey: "id",
-        header: "ID",
-        cell: ({ row }) => {
-            const id: string = row.getValue("id");
+        {
+            accessorKey: "id",
+            header: "ID",
+            cell: ({ row }) => {
+                const id: string = row.getValue("id");
 
-            return (
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <span
-                                onClick={() =>
-                                    navigator.clipboard.writeText(id)
-                                }
-                                className="font-mono border rounded-md p-1"
-                            >
-                                {id}
-                            </span>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p>Click to copy car ID</p>
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-            );
+                return (
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <span
+                                    onClick={() =>
+                                        navigator.clipboard.writeText(id)
+                                    }
+                                    className="font-mono border rounded-md p-1"
+                                >
+                                    {id}
+                                </span>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Click to copy car ID</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                );
+            },
         },
-    },
-    {
-        accessorKey: "model",
-        header: "Model",
-        cell: ({ row }) => {
-            const model: Model = row.getValue("model")
-            const modelName: string = model.name;
+        {
+            accessorKey: "model",
+            header: "Model",
+            cell: ({ row }) => {
+                const model: Model = row.getValue("model")
+                const modelName: string = model.name;
 
-            return (
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <span
-                                onClick={() =>
-                                    navigator.clipboard.writeText(modelName)
-                                }
-                            >
-                                {modelName}
-                            </span>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p>Click to copy car model name</p>
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-            );
+                return (
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <span
+                                    onClick={() =>
+                                        navigator.clipboard.writeText(modelName)
+                                    }
+                                >
+                                    {modelName}
+                                </span>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Click to copy car model name</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                );
+            },
+
+
         },
+        {
+            accessorKey: "location",
+            header: "Location",
+            cell: ({ row }) => {
+                const location: Location = row.getValue("location")
+                const locationCity: string = location.fullAddress.substring(location.fullAddress.indexOf(",") + 2, location.fullAddress.indexOf(",", location.fullAddress.indexOf(",") + 1))
 
+                return (
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <span
+                                    onClick={() =>
+                                        navigator.clipboard.writeText(locationCity)
+                                    }
+                                >
+                                    {locationCity}
+                                </span>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Click to copy the city</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                );
+            }
+        },
+        {
+            accessorKey: "actions",
+            header: "Actions",
+            cell: ({ row }) => {
+                const id: string = row.getValue("id");
+                const model: Model = row.getValue("model")
+                const modelId: string = model.id;
+                const location: Location = row.getValue("location");
+                const locationId: string = location.id;
+                const imageId: string = row.original.imageId;
 
-    },
-    {
-        accessorKey: "location",
-        header: "Location",
-        cell: ({ row }) => {
-            const location: Location = row.getValue("location")
-            const locationCity: string = location.fullAddress.substring(location.fullAddress.indexOf(",") + 2, location.fullAddress.indexOf(",", location.fullAddress.indexOf(",") + 1))
-
-            return (
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <span
-                                onClick={() =>
-                                    navigator.clipboard.writeText(locationCity)
-                                }
-                            >
-                                {locationCity}
-                            </span>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p>Click to copy the city</p>
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-            );
-        }
-    },
-    {
-        accessorKey: "actions",
-        header: "Actions",
-        cell: ({ row }) => {
-            const id: string = row.getValue("id");
-
-            return (
-                <div className="flex gap-4">
-                    <Dialog>
-                        <DialogTrigger asChild>
-                            <Button size="icon" variant="outline">
-                                <Edit />
-                            </Button>
-                        </DialogTrigger>
-                        <DialogContent>
-                            <DialogHeader>
-                                <DialogTitle>Edit Car{" "}
-                                    <span className="font-mono border rounded-md p-1">
-                                        {id}
-                                    </span>
-                                </DialogTitle>
-                            </DialogHeader>
-                        </DialogContent>
-                    </Dialog>
-                    <Button
-                        size="icon"
-                        variant="destructive"
-                        onClick={() => handleDelete(id)}
-                    >
-                        <Trash />
-                    </Button>
-                </div>
-            )
-        }
-    },
-]
+                return (
+                    <div className="flex gap-4">
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button size="icon" variant="outline">
+                                    <Edit />
+                                </Button>
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Edit Car{" "}
+                                        <span className="font-mono border rounded-md p-1">
+                                            {id}
+                                        </span>
+                                    </DialogTitle>
+                                </DialogHeader>
+                                <EditCarForm
+                                    setCarAdded={setCarAdded}
+                                    modelId={modelId}
+                                    locationId={locationId}
+                                    imageId={imageId}
+                                />
+                            </DialogContent>
+                        </Dialog>
+                        <Button
+                            size="icon"
+                            variant="destructive"
+                            onClick={() => handleDelete(id)}
+                        >
+                            <Trash />
+                        </Button>
+                    </div>
+                )
+            }
+        },
+    ]
