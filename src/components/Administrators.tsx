0aeb3a1e1245abc79a5e620ff64f6d016413c9b0
YouRekt <czarnecki.jurko@gmail.com>
2025-02-06 01:40:05 +0100

    feat: replace SidebarButtonProvider with DataTable for Administrators component

--- src/components/Administrators.tsx
+++ src/components/Administrators.tsx
@@ -1,6 +1,11 @@
-import SidebarButtonProvider from "@/components/SidebarButtonProvider";
+import { columns } from "@/components/administrators/columns";
+import { DataTable } from "@/components/administrators/data-table";
 
 const Administrators = () => {
-	return <SidebarButtonProvider>Administrators</SidebarButtonProvider>;
+	return (
+		<div className="container mx-auto py-10 px-4">
+			<DataTable columns={columns} />
+		</div>
+	);
 };
 export default Administrators;
